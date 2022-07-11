79848f37ecd5785748be81314f51c64224e8585d
Dovile-dote <80914644+Dovile-dote@users.noreply.github.com>
2022-07-11 14:33:52 +0300

    front sort

--- src/Components/Front/Front.jsx
+++ src/Components/Front/Front.jsx
@@ -1,5 +1,50 @@
+import Nav from './Nav';
+import List from './List';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { authConfig } from '../../Functions/auth';
+import axios from 'axios';
+import FrontContext from './FrontContext';
+import SortFilter from './SortFilter';
+
 function Front() {
-  return <h1>FRONT</h1>;
+  const [products, setProducts] = useState(null);
+  const [cats, setCats] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3003/products', authConfig())
+      .then((res) => setProducts(res.data.map((p, i) => ({ ...p, row: i }))));
+  }, []);
+
+  // Read categories
+  useEffect(() => {
+    axios
+      .get('http://localhost:3003/cats', authConfig())
+      .then((res) => setCats(res.data));
+  }, []);
+
+  return (
+    <FrontContext.Provider
+      value={{
+        products,
+        setProducts,
+        cats,
+      }}
+    >
+      <Nav />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <SortFilter />
+          </div>
+          <div className="col-12">
+            <List />
+          </div>
+        </div>
+      </div>
+    </FrontContext.Provider>
+  );
 }
 
 export default Front;
---
+++ src/Components/Front/Nav.jsx
@@ -0,0 +1,15 @@
+function Nav() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <nav className="nav">Wecome</nav>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Nav;
